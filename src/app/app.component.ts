323fd62ad9a124acf73bc597b5ad78a431547394
mitsaeirini <mitsaeirini@csd.auth.gr>
2020-03-19 00:09:34 +0200

    workout+homepage+navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,6 @@ import { Router } from '@angular/router';
 })
 export class AppComponent {
   title = 'Tour of Heroes';
-  // constructor(private router: Router) {}
+  logoUrl = './assets/logo.png';
+  imageUrl='./assets/fitness.jpg'
 }
--- src/app/user-main-view/user-main-view.component.ts
+++ src/app/user-main-view/user-main-view.component.ts
@@ -9,7 +9,7 @@ import {Router} from '@angular/router';
 export class UserMainViewComponent implements OnInit {
 
   title = 'Click4Fit';
-  logoUrl = './assets/logo.JPG';
+  logoUrl = './assets/logo.png';
   clickedWorkout = false;
   clickedAbout = false;
   clickedSearch = false;
@@ -19,23 +19,18 @@ export class UserMainViewComponent implements OnInit {
   constructor(private router: Router) {}
 
   goToWorkout() {
-    this.router.navigate(['workout']);
     this.clickedWorkout = true;
   }
   goToSearch() {
-    this.router.navigate(['search']);
     this.clickedSearch = true;
   }
   goToMyprofile() {
-    this.router.navigate(['myprofile']);
     this.clickedMyprofile = true;
   }
   goToAbout() {
-    this.router.navigate(['about']);
     this.clickedAbout = true;
   }
   goToHome() {
-    this.router.navigate(['home']);
     this.clickedHome = true;
   }
 
