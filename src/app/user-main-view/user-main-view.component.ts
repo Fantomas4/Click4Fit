3cea3aff1e9bcdd40f9d0942f020786f746adb67
Fantomas4 <Fantomas4@users.noreply.github.com>
2020-03-20 03:21:35 +0200

    Removed redundant methods from user-main-view.component.ts

--- src/app/user-main-view/user-main-view.component.ts
+++ src/app/user-main-view/user-main-view.component.ts
@@ -9,29 +9,8 @@ import {Router} from '@angular/router';
 export class UserMainViewComponent implements OnInit {
 
   logoUrl = './assets/logo.png';
-  clickedWorkout = false;
-  clickedAbout = false;
-  clickedSearch = false;
-  clickedHome = false;
-  clickedMyprofile = false;
 
   constructor(private router: Router) {}
-
-  goToWorkout() {
-    this.clickedWorkout = true;
-  }
-  goToSearch() {
-    this.clickedSearch = true;
-  }
-  goToMyprofile() {
-    this.clickedMyprofile = true;
-  }
-  goToAbout() {
-    this.clickedAbout = true;
-  }
-  goToHome() {
-    this.clickedHome = true;
-  }
 
   ngOnInit(): void {
   }
