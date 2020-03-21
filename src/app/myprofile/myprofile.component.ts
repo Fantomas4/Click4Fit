5384114b6b89bd9ca141be480e08612246e32e23
mitsaeirini <mitsaeirini@csd.auth.gr>
2020-03-22 00:11:48 +0200

    myprofile

--- src/app/myprofile/myprofile.component.ts
+++ src/app/myprofile/myprofile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {FormControl, Validators} from '@angular/forms';
+import {MyProfileService} from './myprofile.delete.service';
 
 @Component({
   selector: 'app-myprofile',
@@ -7,9 +9,30 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MyprofileComponent implements OnInit {
 
-  constructor() { }
+  firstName: string='Giorgos';
+  lastName: string='Papadopoulos';
+  date = new FormControl(new Date(1997,3,24));
+  registrationEmail: string ='giorgospapad@gmail.com';
+  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
+  initialPassword: string = 'gp123456';
+  repeatedPassword: string = 'gp123456';
+
+  constructor(public deleteService:MyProfileService) { }
 
   ngOnInit(): void {
   }
 
+  getErrorMessage() {
+    if (this.emailFormControl.hasError('required')) {
+      return 'You must enter a value';
+    }
+    return this.emailFormControl.hasError('email') ? 'Not a valid email' : '';
+  }
+  change(){
+
+  }
+  onClick(){
+    this.deleteService.openModal();
+  }
+  
 }
