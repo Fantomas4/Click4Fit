1926cc93f2ad2124af93ecc5cb79d24157ea7bdf
Fantomas4 <Fantomas4@users.noreply.github.com>
2020-04-02 04:10:58 +0300

    Added functional checkboxes column.

--- src/app/manage-business-entries/manage-business-entries.component.ts
+++ src/app/manage-business-entries/manage-business-entries.component.ts
@@ -4,6 +4,7 @@ import {BusinessEntry} from '../business-entry';
 import {MatPaginator} from '@angular/material/paginator';
 import {MatSort} from '@angular/material/sort';
 import {MatTableDataSource} from '@angular/material/table';
+import {SelectionModel} from '@angular/cdk/collections';
 
 
 @Component({
@@ -16,7 +17,9 @@ export class ManageBusinessEntriesComponent implements OnInit {
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
   @ViewChild(MatSort, {static: true}) sort: MatSort;
 
-  displayedColumns = ['id', 'name', 'category'];
+  selection = new SelectionModel<BusinessEntry>(true, []);
+
+  displayedColumns = ['checkboxes', 'id', 'name', 'category'];
 
   businessData: BusinessEntry[];
   dataSource = new MatTableDataSource(this.businessData);
@@ -30,10 +33,42 @@ export class ManageBusinessEntriesComponent implements OnInit {
 
   }
 
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+
+    if (this.dataSource.paginator) {
+      // Return to the first page if not already there
+      this.dataSource.paginator.firstPage();
+    }
+  }
+
   getBusinessEntries() {
 
     this.ManageBusinessEntriesService.getResults()
-      .subscribe(results => {this.businessData = results; this.dataSource.data = this.businessData;});
+      .subscribe(results => {this.businessData = results; this.dataSource.data = this.businessData; });
   }
+
+  /** Checks whether the number of selected elements matches the total number of rows. */
+  isAllSelected() {
+    const numSelected = this.selection.selected.length;
+    const numRows = this.dataSource.data.length;
+    return numSelected === numRows;
+  }
+
+  /** Selects all rows if they are not all selected; otherwise clears selection. */
+  masterToggle() {
+    this.isAllSelected() ?
+      this.selection.clear() :
+      this.dataSource.data.forEach(row => this.selection.select(row));
+  }
+
+  // /** The label for the checkbox on the passed row */
+  // checkboxLabel(row?: BusinessEntry): string {
+  //   if (!row) {
+  //     return `${this.isAllSelected() ? 'select' : 'deselect'} all`;
+  //   }
+  //   return `${this.selection.isSelected(row) ? 'deselect' : 'select'} row ${row.id + 1}`;
+  // }
 
 }
