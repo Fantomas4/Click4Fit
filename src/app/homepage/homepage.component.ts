26e6be648d4c220aa4321c6412ac61831b809709
Alex <alexvava@csd.auth.gr>
2020-04-17 13:47:35 +0200

    Started toolbar hidding in user view

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-homepage',
@@ -8,7 +9,14 @@ import { Component, OnInit } from '@angular/core';
 export class HomepageComponent implements OnInit {
   logoUrl = '../assets/logo.png';
 
-  constructor() { }
+  screenWidth: number;
+
+  constructor(private router: Router) {
+    this.screenWidth = window.innerWidth;
+    window.onresize = () => {
+      this.screenWidth = window.innerWidth;
+    };
+  }
 
   ngOnInit(): void {
   }
