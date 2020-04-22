1659b43223df8dabbeff147b1b415d51572055af
Alex <alexvava@csd.auth.gr>
2020-04-23 01:00:18 +0200

    Added Comments to home
    Deleted old code and added some comments

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,5 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-// import {NgbCarouselConfig} from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-home',
@@ -8,31 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-
-  picture1 = './assets/picture1.jpg';
-  picture2 = './assets/picture2.jpg';
-  picture3 = './assets/picture3.jpg';
-  height: number;
-  width: number;
-
-  // constructor(config: NgbCarouselConfig) {
-  //   config.interval = 2100;
-  //   config.wrap = true;
-  //   config.keyboard = false;
-  //   config.pauseOnHover = true;
-  //   config.showNavigationArrows = false;
-  //   config.showNavigationIndicators = true;
-  //   this.height = screen.height - 192;
-  //   this.width = screen.width;
-  // }
+  constructor() { }
 
   ngOnInit(): void {
-  }
-  getWidth() {
-      return this.width;
-  }
-  getHeight() {
-    return this.height;
   }
 
 }
