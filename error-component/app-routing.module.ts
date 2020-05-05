85df7c4cb7096fa3d7573e2f0d3519b13a5dc5f2
mkokkoni <32477051+mkokkoni@users.noreply.github.com>
2020-05-05 21:46:14 +0300

    Add files via upload
    error component implementation

---
+++ error-component/app-routing.module.ts
@@ -0,0 +1,46 @@
+import { NgModule } from '@angular/core';
+import { Routes } from '@angular/router';
+import { RouterModule } from '@angular/router';
+import {WorkoutComponent} from '../workout/workout.component';
+import {MyprofileComponent} from '../myprofile/myprofile.component';
+import {SearchComponent} from '../search/search.component';
+import {AboutComponent} from '../about/about.component';
+import {DashboardComponent} from '../dashboard/dashboard.component';
+import {LoginComponent} from '../login/login.component';
+import {RegisterComponent} from '../register/register.component';
+import {RecoverPasswordComponent} from '../recover-password/recover-password.component';
+import {UserMainViewComponent} from '../user-main-view/user-main-view.component';
+import {ContactUsComponent} from '../contact-us/contact-us.component';
+import {HomeComponent} from '../home/home.component';
+import {ManageBusinessEntriesComponent} from '../manage-business-entries/manage-business-entries.component';
+import {ManageUserEntriesComponent} from '../manage-user-entries/manage-user-entries.component';
+import {ErrorPageComponent} from '../error-page/error-page.component';
+
+const routes: Routes = [
+  {path: '', redirectTo: '/home', pathMatch: 'full' },
+  {path: 'home', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'contact-us', component: ContactUsComponent},
+  {path: 'login', component: LoginComponent},
+  {path: 'error-page', component: ErrorPageComponent},
+  {path: 'recover-password', component: RecoverPasswordComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'user', component: UserMainViewComponent,
+    children: [
+      { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+      { path: 'manage-user-entries', component: ManageUserEntriesComponent },
+      { path: 'manage-business-entries', component: ManageBusinessEntriesComponent},
+      {path: 'dashboard', component: DashboardComponent},
+      {path: 'myprofile', component: MyprofileComponent},
+      {path: 'search', component: SearchComponent},
+      {path: 'workout', component: WorkoutComponent }
+    ]},
+  {path: 'about', component: AboutComponent},
+];
+
+@NgModule({
+  declarations: [],
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ error-component/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+
+  constructor(private router: Router) {
+    // any misspelled or non existing url/error that occurs, redirects to error-page
+    this.router.errorHandler = (error: any) => {
+      const routerError = error.toString();
+      if (routerError.indexOf('Cannot match any routes') >= 0 ) {
+        this.router.navigate(['/error-page']);
+      } else {
+        throw error;
+      }
+    };
+  }
+  // logoUrl = './assets/logo.png';
+}
