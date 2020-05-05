2ecf972c126520195fba638e3595fdd9440b4d8f
Μαρινα Κοκκωνη <mkokkoni@csd.auth.gr>
2020-05-05 21:24:11 +0300

    database for users and business in python

---
+++ back-end/MockDatabase.py
@@ -0,0 +1,127 @@
+datastore = {
+        'user':[
+           { 'name': 'Nikos', 'surname': 'Alexopoulos', 'email': 'nikosalex@gmail.com', 'password': 'gp123456', 'birthdate': '04,16,1997', 'role': '', 'favorites': '', 'id': '1'  },
+           { 'name': 'Giorgos', 'surname': 'Andrianakis', 'email': 'gandrian@gmail.com',  'password': 'ga123456', 'birthdate': '06,02,1961' , 'role': '', 'favorites': '', 'id': '2' },
+           { 'name': 'Angelina', 'surname': 'Athanasiou', 'email': 'angath@gmail.com', 'password':'an123456','birthdate': '01,07,1991', 'role': '', 'favorites': '', 'id': '3' },
+           { 'name': 'Kostas', 'surname': 'Bakirtzis', 'email': 'kostasba@gmail.com', 'password':'an123456', 'birthdate': '01,25,1998' , 'role': '', 'favorites': '', 'id': '4' },
+           { 'name': 'Eirini', 'surname': 'Christoforou', 'email': 'chriseir@hotmail.com', 'password':'ec123456',  'birthdate': '08,3,1967', 'role': '', 'favorites': '', 'id': '5' },
+           { 'name': 'Agapi', 'surname': 'Christoyannopoulou', 'email': 'chrisagapi@gmail.com', 'password':'ac123456', 'birthdate': '04,30,2003', 'role': '', 'favorites': '', 'id': '6' },
+           { 'name': 'Giannis', 'surname': 'Chronis', 'email': 'giannis@gmail.com', 'password':'gc123456', 'birthdate': '03,05,1977' , 'role': '', 'favorites': '', 'id': '7'  },
+           { 'name': 'Vasilis', 'surname': 'Calimeris', 'email': 'vcalime@gmail.com', 'password':'vc123456', 'birthdate': '05,22,2000', 'role': '', 'favorites': '', 'id': '8'  },
+           { 'name': 'Panagiotis', 'surname': 'Danielopoulos', 'email': 'pandaniel@gmail.com', 'password':'pd123456', 'birthdate': '09,10,1991', 'role': '', 'favorites': '', 'id': '9' },
+           { 'name': 'Sakis', 'surname': 'Rouvas', 'email': 'sakisr@gmail.com', 'password':'sr123456', 'birthdate': '07,22,2000', 'role': '', 'favorites': '', 'id': '10'  },
+           { 'name': 'Alexandros', 'surname': 'Diamandis', 'email': 'diamalex@gmail.com', 'password':'ad123456', 'birthdate': '11,17,1995', 'role': '', 'favorites': '', 'id': '11' },
+           { 'name': 'Nicolas', 'surname': 'Economos', 'email': 'nikecon@gmail.com', 'password':'ne123456', 'birthdate': '08,22,1980', 'role': '', 'favorites': '', 'id': '12' },
+           { 'name': 'Maria', 'surname': 'Papadopoulou', 'email': 'mariapap@hotmail.com', 'password':'mp123456', 'birthdate': '04,12,2002', 'role': '', 'favorites': '', 'id': '13' },
+           { 'name': 'Heleni', 'surname': 'Frangopoulou', 'email': 'helenfrang@gmail.com', 'password':'hf123456', 'birthdate': '11,12,1969', 'role': '', 'favorites': '', 'id': '14' },
+           { 'name': 'Dimitris', 'surname': 'Ganas', 'email': 'dimigan@gmail.com', 'password':'dg123456', 'birthdate': '12,28,1988', 'role': '', 'favorites': '', 'id': '15' },
+           { 'name': 'Olga', 'surname': 'Hatzi', 'email': 'hatziolg@gmail.com', 'password':'oh123456', 'birthdate': '06,06,1981', 'role': '', 'favorites': '', 'id': '16' },
+           { 'name': 'Giannis', 'surname': 'Iraklidis', 'email': 'iraklg@gmail.com', 'password':'gi123456', 'birthdate': '03,02,1990', 'role': '', 'favorites': '', 'id': '17' },
+           { 'name': 'Giorgos', 'surname': 'Iordanou', 'email': 'iordgiorg@gmail.com', 'password':'gi123456', 'birthdate': '06,21,1991', 'role': '', 'favorites': '', 'id': '18' },
+           { 'name': 'Dimitris', 'surname': 'Karras', 'email': 'dimkarras@gmail.com', 'password':'dk123456', 'birthdate': '07,24,1982', 'role': '', 'favorites': '', 'id': '19' },
+           { 'name': 'Athanasios', 'surname': ' Karagiannis', 'email': 'athkara@gmail.com', 'password':'ak123456', 'birthdate': '10,09,1998', 'role': '', 'favorites': '', 'id': '20' },
+           { 'name': 'Maria', 'surname': 'Metaxa', 'email': 'mariamet@gmail.com', 'password':'mm123456', 'birthdate': '07,25,1989', 'role': '', 'favorites': '', 'id': '21' },
+           { 'name': 'Marianna', 'surname': 'Nikolaou', 'email': 'marianik@gmail.com', 'password':'mn123456', 'birthdate': '05,07,2003', 'role': '', 'favorites': '', 'id': '22' },
+           { 'name': 'Kostas', 'surname': 'Papazoglou', 'email': 'kostpap@hotmail.com', 'password':'kp123456', 'birthdate': '10,02,1974', 'role': '', 'favorites': '', 'id': '23' },
+           { 'name': 'Apostolos', 'surname': 'Sarris', 'email': 'aposarr@gmail.com', 'password':'as123456', 'birthdate': '04,19,2004', 'role': '', 'favorites': '', 'id': '24' },
+           { 'name': 'Sofia', 'surname': 'Yiannopoulou', 'email': 'sofiay@gmail.com', 'password':'sy123456', 'birthdate': '07,10,2000', 'role': '', 'favorites': '', 'id': '25' },
+           { 'name': 'Heleni', 'surname': 'Zika', 'email': 'hzika@gmail.com', 'password':'hz123456', 'birthdate': '08,27,1966', 'role': '', 'favorites': '', 'id': '26' },
+           { 'name': 'Stella', 'surname': 'Zafeiriou', 'email': 'zafeistella@gmail.com', 'password':'sz123456', 'birthdate': '01,03,1993', 'role': '', 'favorites': '', 'id': '27' },
+           { 'name': 'Vasillis', 'surname': 'Vasilakis', 'email': 'vasillisvas@gmail.com', 'password':'vv123456', 'birthdate': '02,02,2001', 'role': '', 'favorites': '', 'id': '28' },
+           { 'name': 'Varvara', 'surname': ' Stefanopoulou', 'email': 'stefvar@gmail.com', 'password':'vs123456', 'birthdate': '06,30,1973', 'role': '', 'favorites': '', 'id': '29' },
+           { 'name': 'Silvia', 'surname': 'Sideri', 'email': 'sidersilv@gmail.com', 'password':'ss123456', 'birthdate': '05,25,1969', 'role': '', 'favorites': '', 'id': '30' }
+        ],
+        'business': [
+             { 'name': 'FitClub', 'category': 'Gym', 'country': 'Greece', 'cities': 'Thessaloniki', 'address': 'diagora 20', 'postal_code': '567 55',
+               'phone_number': '2310 634590', 'email': 'fitclub@gmail.com', 'img_Path': './assets/gym-preview.JPG', 'services': ['service_1', 'service_2'],
+               'products': ['product_1', 'product_2'], 'id': '1' },
+
+             { 'name': 'Planet Fitness', 'category': 'Gym', 'country': 'Greece', 'city': 'Athens', 'address': '3 Septembriou Street 6', 'postal_code': '576 45',
+               'phone_number': '202-555-0376', 'email': 'planetfitness@gmail.com', 'img_Path': './assets/gym-preview.JPG', 'services': ['service_1', 'service_2'],
+               'products': ['product_1', 'product_2', 'service_1', 'service_2'], 'id': '2'},
+
+             { 'name': 'Giorgos Anastasiou', 'category': 'Personal Trainer', 'country': 'Greece', 'city': 'Xanthi', 'address': 'Adrianou Street 12', 'postal_code': '587 05',
+               'phone_number': '202-555-0227', 'email': 'anastgiog@gmail.com', 'img_Path': './assets/gym-preview.JPG', 'services': ['service_1', 'service_2'],
+               'products': ['product_1', 'product_2'], 'id': '3' },
+
+             { 'name': 'Hybrid Fitness', 'category': 'Gym', 'country': 'Greece', 'city': 'Thessaloniki', 'address': 'Aiolou Street 45', 'postal_code': '534 85',
+               'phone_number': '202-555-0004', 'email': 'hybridfitness@gmail.com', 'img_Path': './assets/gym-preview.JPG',  'services': ['service_1', 'service_2'],
+               'products': ['product_1', 'product_2'], 'id': '4'  },
+
+             { 'name': 'Mindful Body Fitness', 'category': 'Fitness Shop', 'country': 'Greece', 'city': 'Kavala', 'address': 'Akadimias Street', 'postal_code': '515 75',
+               'phone_number': '202-555-0781', 'email': 'mindful@gmail.com', 'img_Path': './assets/gym-preview.JPG', 'services': ['service_1', 'service_2'],
+               'products': ['product_1', 'product_2'], 'id': '5'  },
+
+             { 'name': 'CrossFit Gym', 'category': 'Gym', 'country': 'Greece', 'city': 'Thessaloniki', 'address': 'Benaki Street 4', 'postal_code': '567 00',
+               'phone_number': '202-555-0486', 'email': 'crossfit@gmail.com', 'img_Path': './assets/gym-preview.JPG',  'services': ['service_1', 'service_2'],
+               'products': ['product_1', 'product_2'], 'id': '6' },
+
+             { 'name': 'Movati Athletic', 'category': 'Gym', 'country': 'Greece', 'city': 'Athens', 'address': 'Chalkokondyli Street 7', 'postal_code': '552 99',
+               'phone_number': '202-555-0722', 'email': 'movatiathletic@gmail.com', 'img_Path': './assets/gym-preview.JPG', 'services': ['service_1', 'service_2'],
+               'products': ['product_1', 'product_2'], 'id': '7' },
+
+             { 'name': 'Vasillis Baskos', 'category': 'Personal Trainer', 'country': 'Greece', 'city': 'Kavala', 'address': 'Evripidou Street', 'postal_code': '670 00',
+               'phone_number': '202-555-0406', 'email': 'vasillisb@gmail.com', 'img_Path': './assets/gym-preview.JPG', 'services': ['service_1', 'service_2'],
+               'products': ['product_1', 'product_2'], 'id': '8' },
+
+             { 'name': 'Anytime Fitness', 'category': 'Gym', 'country': 'Greece', 'city': 'Thessaloniki', 'address': 'Ioanninon Street 34', 'postal_code': '778 66',
+               'phone_number': '202-555-0537', 'email': 'anytime@gmail.com', 'img_Path': './assets/gym-preview.JPG', 'services': ['service_1', 'service_2'],
+               'products': ['product_1', 'product_2'], 'id': '9' },
+
+             { 'name': 'Six Pack Abs Class Room', 'category': 'Fitness Shop', 'country': 'Greece', 'city': 'Thessaloniki', 'address': 'Lada Street 77', 'postal_code': '445 00',
+               'phone_number': '202-555-0331', 'email': 'sixabs@gmail.com', 'img_Path': './assets/gym-preview.JPG', 'services': ['service_1', 'service_2'],
+               'products': ['product_1', 'product_2'], 'id': '10' },
+
+             { 'name': 'Adrenaline Training', 'category': 'Gym', 'country': 'Greece', 'city': 'Thessaloniki', 'address': 'Larissis Street 3', 'postal_code': '457 88',
+               'phone_number': '202-555-0735', 'email': 'andrenaline@gmail.com', 'img_Path': './assets/gym-preview.JPG', 'services': ['service_1', 'service_2'],
+               'products': ['product_1', 'product_2'], 'id': '11' },
+
+             { 'name': 'Rush Hour', 'category': 'Gym', 'country': 'Greece', 'city': 'Athens', 'address': 'Marni Street 44', 'postal_code': '333 78',
+               'phone_number': '202-555-0664', 'email': 'rushhour@gmail.com', 'img_Path': './assets/gym-preview.JPG', 'services': ['service_1', 'service_2'],
+               'products': ['product_1', 'product_2'], 'id': '12' },
+
+             { 'name': 'Niki Karaila', 'category': 'Personal Trainer', 'country': 'Greece', 'city': 'Athens', 'address': 'Palamidiou Street 6', 'postal_code': '233 67',
+               'phone_number': '202-555-0819', 'email': 'nikik@gmail.com', 'img_Path': './assets/gym-preview.JPG', 'services': ['service_1', 'service_2'],
+               'products': ['product_1', 'product_2'], 'id': '13' },
+
+             { 'name': 'Fitclub Bootcamps', 'category': 'Gym', 'country': 'Greece', 'city': 'Thessaloniki', 'address': 'Petrou Ralli Street 77', 'postal_code': '667 00',
+               'phone_number': '202-555-0747', 'email': 'fitclub@gmail.com', 'img_Path': './assets/gym-preview.JPG', 'services': ['service_1', 'service_2'],
+               'products': ['product_1', 'product_2'], 'id': '14' },
+
+             { 'name': 'Your Wellbeing', 'category': 'Fitness Shop', 'country': 'Greece', 'city': 'Thessaloniki', 'address': 'Voreou Street 66', 'postal_code': '888 32',
+               'phone_number': '202-555-0747', 'email': 'yourwellbeing@gmail.com', 'img_Path': './assets/gym-preview.JPG', 'services': ['service_1', 'service_2'],
+               'products': ['product_1', 'product_2'], 'id': '15' },
+
+             { 'name': 'Restore Total Wellness', 'category': 'Fitness Shop', 'country': 'Greece', 'city': 'Athens', 'address': 'Sepolion Street 45', 'postal_code': '907 55',
+               'phone_number': '202-555-0735', 'email': 'restore@gmail.com', 'img_Path': './assets/gym-preview.JPG', 'services': ['service_1', 'service_2'],
+               'products': ['product_1', 'product_2'], 'id': '16' },
+
+             { 'name': 'Anna Lazarou', 'category': 'Personal Trainer', 'country': 'Greece', 'city': 'Thessaloniki', 'address': 'Vyssis Street 56', 'postal_code': '667 00',
+               'phone_number': '307-555-0188', 'email': 'annalaz@gmail.com', 'img_Path': './assets/gym-preview.JPG', 'services': ['service_1', 'service_2'],
+               'products': ['product_1', 'product_2'], 'id': '17'  },
+
+             { 'name': 'Athletic Fluency', 'category': 'Gym', 'country': 'Greece', 'city': 'Athens', 'address': 'Stadiou Street 12', 'postal_code': '128 02',
+               'phone_number': '307-555-0148', 'email': 'athleticfluency@gmail.com', 'img_Path': './assets/gym-preview.JPG',  'services': ['service_1', 'service_2'],
+               'products': ['product_1', 'product_2'], 'id': '18'  },
+
+             { 'name': 'Wellness Rediscovery', 'category': 'Fitness Shop', 'country': 'Greece', 'city': 'Kavala', 'address': 'Petrou Ralli Street 12', 'postal_code': '866 32',
+               'phone_number': '307-555-0106', 'email': 'wellnessred@gmail.com', 'img_Path': './assets/gym-preview.JPG', 'services': ['service_1', 'service_2'],
+               'products': ['product_1', 'product_2'], 'id': '19'  },
+
+             { 'name': 'PureGym', 'category': 'Gym', 'country': 'Greece', 'city': 'Thessaloniki', 'address': 'Stavrou Street 17', 'postal_code': '564 32',
+               'phone_number': '307-555-0184', 'email': 'yourwellbeing@gmail.com', 'img_Path': './assets/gym-preview.JPG',  'services': ['service_1', 'service_2'],
+               'products': ['product_1', 'product_2'], 'id': '20'  }
+        ]
+
+
+
+}
+
+// datastore["user"]["name"] //
+
+
+
+
+
+
+
