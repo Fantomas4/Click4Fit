c330c3a62eb39627574183257b31789737dcf5c5
Alex <alexvava@csd.auth.gr>
2020-05-09 20:13:13 +0200

    Validator Functional
    Need to add comments to MongoDB methods and add more tests to ValidatorTests

--- back-end/Validator.py
+++ back-end/Validator.py
@@ -3,116 +3,122 @@ import re
 
 class Validator:
 
-    valid = {
-        "user" : {
-            "legal-attributes" : ["name", "surname", "email", "password", "birthdate", "role",
-                                "favorite_business", "favorite_workout", "id", "session_id"],
-            "type" : {
-                "_id"                : str,
-                "name"               : str,
-                "surname"            : str,
-                "email"              : str,
-                "password"           : str,
-                "birthdate"          : str,
-                "role"               : str,
-                "favorite_business"  : list,
-                "favorite_workout"   : list,
-                "session_id"         : str
-            },
-            "regex" : {
-                "name"      : r"[A-Za-z]{2,25}( [A-Za-z]{2,25})?",
-                "surname"   : r"[A-Za-z]{2,25}",
-                "email"     : r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)",
-                "password"  : r'[A-Za-z0-9@#$%^&+=]{8,}',
-                "birthdate" : r"^([1-9]|0?[1-9]|1[0-9]|2[0-9]|3[0-1])(/|\.|-|,)([1-9]|0?[1-9]|1[0-2])(/|\.|-|,)(20[0-9][0-9]|1[0-9][0-9][0-9])$",
-                "role"      : r"(admin|client|business)"
-            },
-            "regex-error" : {
-                "name"      : "must only contain letters from a-z and A-Z and length can be from 2 to 25 characters also accepts two names eg. Georgios Alexandros",
-                "surname"   : "must only contain letters from a-z and A-Z and length can be from 2 to 25 characters",
-                "email"     : "must be of fromat local_part@domain_part, local_part can only contain these special characters: _.+-",
-                "password"  : "must be at least of length 8 and contain letters from a-z and A-Z numbers and speceial characters: @#$%^&+=",
-                "birthdate" : "must be of format dd.mm.yyyy or dd/mm/yyyy or dd-mm-yyyy or dd,mm,yyyy and must range between the year 1900 and 2099",
-                "role"      : "can only take one of these values : \"admin\", \"client\", \"business\""
-            }
-        },
-        "business" : {
-            "legal-attributes" : ["name", "category", "country", "city", "address", "postal_code", "phone_number",
-                                    "email", "img_path", "services", "products", "id"],
-            "type" : {
-                "_id"          : str,
-                "name"         : str,
-                "category"     : str,
-                "country"      : str,
-                "city"         : str,
-                "address"      : str,
-                "postal_code"  : str,
-                "phone_number" : str,
-                "email"        : str,
-                "img_path"     : str,
-                "services"     : list,
-                "products"     : list
-            },
-            "regex" : {
-                "email"    : r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)",
-                "category" : r"(gym|personal trainer|fitness shop)"
-            },
-            "regex-error" : {
-                "email"    : "must be of fromat local_part@domain_part, local_part can only contain these special characters: _.+-",
-                "category" : "can only take one of these value:  \"gym\", \"personal trainer\", \"fitness shop\""
-            }
-        },
-        "workout" : {
-            "legal-attributes" : ["name", "main_group", "muscle_groups", "advised_for", "difficulty",
-                                "equipment", "sets", "video_url", "id"],
-            "type" : {
-                "_id"           : str,
-                "name"          : str,
-                "main_group"    : str,
-                "muscle_groups" : list, 
-                "advised_for"   : str,
-                "difficulty"    : str,
-                "equipment"     : bool,
-                "sets"          : str,
-                "video_url"     : str 
-            },
-            "regex" : {
-                "advised_for" : r"(men|women)",
-                "difficulty"  : r"(easy|medium|hard)",
-            },
-            "regex-error" : {
-                "advised_for" : "can only be one of these values: \"men\", \"women\"",
-                "difficulty"  : "can only be one of these values: \"easy\", \"medium\", \"hard\""
-            }
+    def __init__(self):
 
+        self.valid = {
+            "user" : {
+                "legal-attributes" : ["name", "surname", "email", "password", "birthdate", "role",
+                                    "favorite_business", "favorite_workout", "id", "session_id"],
+                "type" : {
+                    "_id"                : str,
+                    "name"               : str,
+                    "surname"            : str,
+                    "email"              : str,
+                    "password"           : str,
+                    "birthdate"          : str,
+                    "role"               : str,
+                    "favorite_business"  : list,
+                    "favorite_workout"   : list,
+                    "session_id"         : str
+                },
+                "regex" : {
+                    "name"      : r"[A-Za-z]{2,25}( [A-Za-z]{2,25})?",
+                    "surname"   : r"[A-Za-z]{2,25}",
+                    "email"     : r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)",
+                    "password"  : r'[A-Za-z0-9@#$%^&+=]{8,}',
+                    "birthdate" : r"^([1-9]|0?[1-9]|1[0-9]|2[0-9]|3[0-1])(/|\.|-|,)([1-9]|0?[1-9]|1[0-2])(/|\.|-|,)(20[0-9][0-9]|1[0-9][0-9][0-9])$",
+                    "role"      : r"(admin|client|business)"
+                },
+                "regex-error" : {
+                    "name"      : "must only contain letters from a-z and A-Z and length can be from 2 to 25 characters also accepts two names eg. Georgios Alexandros",
+                    "surname"   : "must only contain letters from a-z and A-Z and length can be from 2 to 25 characters",
+                    "email"     : "must be of fromat local_part@domain_part, local_part can only contain these special characters: _.+-",
+                    "password"  : "must be at least of length 8 and contain letters from a-z and A-Z numbers and speceial characters: @#$%^&+=",
+                    "birthdate" : "must be of format dd.mm.yyyy or dd/mm/yyyy or dd-mm-yyyy or dd,mm,yyyy and must range between the year 1900 and 2099",
+                    "role"      : "can only take one of these values : \"admin\", \"client\", \"business\""
+                }
+            },
+            "business" : {
+                "legal-attributes" : ["name", "category", "country", "city", "address", "postal_code", "phone_number",
+                                        "email", "img_path", "services", "products", "id"],
+                "type" : {
+                    "_id"          : str,
+                    "name"         : str,
+                    "category"     : str,
+                    "country"      : str,
+                    "city"         : str,
+                    "address"      : str,
+                    "postal_code"  : str,
+                    "phone_number" : str,
+                    "email"        : str,
+                    "img_path"     : str,
+                    "services"     : list,
+                    "products"     : list
+                },
+                "regex" : {
+                    "email"    : r"(^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$)",
+                    "category" : r"(gym|personal trainer|fitness shop)"
+                },
+                "regex-error" : {
+                    "email"    : "must be of fromat local_part@domain_part, local_part can only contain these special characters: _.+-",
+                    "category" : "can only take one of these value:  \"gym\", \"personal trainer\", \"fitness shop\""
+                }
+            },
+            "workout" : {
+                "legal-attributes" : ["name", "main_group", "muscle_groups", "advised_for", "difficulty",
+                                    "equipment", "sets", "video_url", "id"],
+                "type" : {
+                    "_id"           : str,
+                    "name"          : str,
+                    "main_group"    : str,
+                    "muscle_groups" : list, 
+                    "advised_for"   : str,
+                    "difficulty"    : str,
+                    "equipment"     : bool,
+                    "sets"          : str,
+                    "video_url"     : str 
+                },
+                "regex" : {
+                    "advised_for" : r"(men|women)",
+                    "difficulty"  : r"(easy|medium|hard)",
+                },
+                "regex-error" : {
+                    "advised_for" : "can only be one of these values: \"men\", \"women\"",
+                    "difficulty"  : "can only be one of these values: \"easy\", \"medium\", \"hard\""
+                }
+
+            }
         }
-    }
 
     
-    def validate(self, dict: dict, db: str):
+    def validate(self, json_dict: dict, db: str):
+        if type(json_dict) is not dict:
+            raise TypeError("json_dict must be of type dict and got " + str(type(json_dict)))
+        if not json_dict:
+            raise ValueError("json_dict is empty")
         if type(db) is not str:
             raise TypeError("db must be of type str and got " + str(type(db)))
         if db not in ["user", "business", "workout"]:
             raise ValueError("db can only take one of these values (user, business, workout) and got: "
                                 + db + " instead")
-        for key in dict.keys():
+        for key in json_dict.keys():
             # checking attribute type
             if type(key) is not str:
                 raise TypeError(db + " attribute names must be of type str and got "
                                 + str(type(key)))
             # checking for illegal attributes
-            if key not in valid[db]["legal-attributes"]:
+            if key not in self.valid[db]["legal-attributes"]:
                 raise ValueError(db + " contains invalid attribute name: " + key)
             # checking value type
-            if type(dict[key]) is not valid[db]["type"][key]:
+            if type(json_dict[key]) is not self.valid[db]["type"][key]:
                 raise TypeError("""
                 {} attribute must be of type {} and got {} instead
-                """.format(key, str(valid[db]["type"][key]), str(type(dict[key]))))
+                """.format(key, str(self.valid[db]["type"][key]), str(type(json_dict[key]))))
             # checking value format if available reggex available
-            if not re.fullmatch(valid[db]["regex"].get(key, r"."), dict[key]):
+            if not re.fullmatch(self.valid[db]["regex"].get(key, r"."), json_dict[key]):
                 raise ValueError("""
                 invalid {0} ({2}): {0} {1}
-                """.format(key, valid[db]["regex-error"][key], dict[key]))
+                """.format(key, self.valid[db]["regex-error"][key], json_dict[key]))
 
 
 
@@ -161,8 +167,8 @@ class Validator:
 
 
 
-    # def valid_user(self, user: dict):
-    #     if type(user) is not dict: raise TypeError("user: expected dict and got " + str(type(user)))
+    # def valid_user(self, user: json_dict):
+    #     if type(user) is not json_dict: raise TypeError("user: expected json_dict and got " + str(type(user)))
     #     if not user: raise ValueError("user is empty")
     #     for key in user.keys():
     #         if type(key) is not str: raise TypeError("user attribute names must be of type str and got " + str(type(key)))
@@ -191,8 +197,8 @@ class Validator:
     #             raise ValueError("""invalid {0} ({2}): {0} {1}""".format(key, user_attribute_format[key], user[key]))
     
 
-    # def valid_business(self, business: dict):
-    #     if type(business) is not dict: raise TypeError("business: expected dict and got " + str(type(business)))
+    # def valid_business(self, business: json_dict):
+    #     if type(business) is not json_dict: raise TypeError("business: expected json_dict and got " + str(type(business)))
     #     if not business: raise ValueError("business is empty")
     #     for key in business.keys():
     #         if type(key) is not str: raise TypeError("business attribute names must be of type str and got " + str(type(key)))
