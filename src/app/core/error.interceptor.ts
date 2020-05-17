15d1f738b081001674e16114569127c9c0aecf85
Fantomas4 <stefanokf@auth.gr>
2020-05-17 07:07:07 +0300

    Commented out the code line in error interceptor responsible to log out the current user if a 401 http status code is received.

--- src/app/core/error.interceptor.ts
+++ src/app/core/error.interceptor.ts
@@ -14,9 +14,9 @@ export class ErrorInterceptor implements HttpInterceptor {
       console.log('ERROR INTERCEPTOR - status: ', err.status);
       console.log('ERROR INTERCEPTOR - err:', err);
       if (err.status === 401) {
-        // auto logout if 401 response returned from api
-        this.authenticationService.logout();
-        location.reload(true);
+        // // auto logout if 401 response returned from api
+        // this.authenticationService.logout();
+        // location.reload(true);
       }
 
       // const error = err.error.message || err.statusText;
