e2861f5d5218623393c5c02d46543e7c1b4a5c8b
Fantomas4 <stefanokf@auth.gr>
2020-05-17 19:26:20 +0300

    Fixed error during build caused by wrong variable name.

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -1,8 +1,6 @@
-
 import { Component, OnInit } from '@angular/core';
 import {FormControl, Validators} from '@angular/forms';
 import {ContactUsService} from './contact-us.service';
-import {FormControl, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-contact-us',
@@ -10,7 +8,7 @@ import {FormControl, Validators} from '@angular/forms';
   styleUrls: ['./contact-us.component.css']
 })
 export class ContactUsComponent implements OnInit {
-  title = "Contact Us"
+  title = 'Contact Us';
   content;
 
   emailFormControl = new FormControl('', [Validators.required, Validators.email]);
@@ -21,15 +19,14 @@ export class ContactUsComponent implements OnInit {
   textarea: string;
   subject: string;
 
-  constructor(public contactUsService: ContactUsService) {
-  }
+  constructor(public contactUsService: ContactUsService) {}
 
   ngOnInit(): void {
   }
 
   onClick() {
     this.content={'fullname':this.fullName,'email':this.contactEmail,'telephone':this.telephone,'subject':this.subject,'text':this.textarea};
-    this.contanctUsService.postDetails(this.content);
-    this.contanctUsService.openModal();
+    this.contactUsService.postDetails(this.content);
+    this.contactUsService.openModal();
   }
 }
