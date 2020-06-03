04f468077a02d1c96cbdee1280b2d3fbb725eeb7
Alex <alexvava@csd.auth.gr>
2020-06-03 15:23:13 +0200

    implemented upload endpoint

--- back-end/Rest/flaskAPI.py
+++ back-end/Rest/flaskAPI.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask, jsonify,request,json, flash, redirect, url_for
+from flask import Flask, jsonify, request, json
 from werkzeug.utils import secure_filename
 from flask_cors import CORS
 from pprint import pprint
@@ -14,12 +14,12 @@ from MongoDatabase.Wrappers.WorkoutListWrapper import WorkoutListWrapper
 from MongoDatabase.Wrappers.WorkoutWrapper import WorkoutWrapper
 from MongoDatabase.Wrappers.UserListWrapper import UserListWrapper
 
-UPLOAD_FOLDER = './uploads'
 ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 APP_ROOT = os.path.dirname(os.path.abspath(__file__))
+UPLOAD_FOLDER = os.path.dirname(__file__) + "\\uploads"
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 MongoDB=MongoDB()
 CORS(app)
 
@@ -33,21 +33,19 @@ def allowed_file(filename):
 def upload_file():
     if request.method == 'POST':
         # check if the post request has the file part
+        # print(request.get_json())
         if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
+            return "No file part", 422
         file = request.files['file']
         # if user does not select file, browser also
         # submit an empty part without filename
         if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
+            return "No selected file", 422
         if file and allowed_file(file.filename):
             filename = secure_filename(file.filename)
             file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-            return redirect(url_for('uploaded_file',
-                                    filename=filename))
-    return "Image Upload Successfull", 200
+            return "Image Upload Successfull", 200
+    return "Not a post request", 422
 
 ####################################### Contact Us ###################################
 @app.route("/api/contactus",methods=['POST','GET'])
