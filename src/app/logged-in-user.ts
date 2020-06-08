2cf619387b98613d8385a5ec0c9f1d93a06b1f73
Stefanos Karamperas <stef1997s@gmail.com>
2020-06-08 19:04:51 +0300

    Started work on changes for authentication.service.ts

--- src/app/logged-in-user.ts
+++ src/app/logged-in-user.ts
@@ -3,6 +3,9 @@
  * of the user currently logged in.
  */
 export interface LoggedInUser {
+  _id: string;
+  name: string;
+  surname: string;
   email: string;
   privilegeLevel: string; // Used to determine the level of access privileges a user has (client, admin).
   token: string; // Used to hold the JWT token that is returned from the api on successful authentication.
