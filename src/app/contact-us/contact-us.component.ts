658dc36e7a1266b83e9eb8ec76a996f42be03d5c
mitsaeirini <mitsaeirini@csd.auth.gr>
2020-06-16 16:29:03 +0300

    add validator to contact ud

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -1,30 +1,51 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, Validators} from '@angular/forms';
+import {FormControl, Validators, FormGroup, FormGroupDirective, NgForm} from '@angular/forms';
 import { AlertService } from '../core/alert.service';
 import { Subscription } from 'rxjs';
 import {ContactUsService} from './contact-us.service';
+import { ErrorStateMatcher } from '@angular/material/core';
 
 interface AlertMessage {
   type: string;
   text: string;
 }
-
+export class GenericErrorStateMatcher implements ErrorStateMatcher {
+  isErrorState(control: FormControl | null, form: FormGroupDirective | NgForm | null): boolean {
+    // console.log(control);
+    // console.log(form);
+    const isSubmitted = form && form.submitted;
+    return !!(control && control.invalid && (control.dirty || control.touched || isSubmitted));
+  }
+}
 @Component({
   selector: 'app-contact-us',
   templateUrl: './contact-us.component.html',
   styleUrls: ['./contact-us.component.css']
 })
 export class ContactUsComponent implements OnInit {
+  
+  entryForm = new FormGroup( {
+    fullName: new FormControl('', [
+      Validators.required
+    ]),
+    email: new FormControl('', [
+      Validators.required,
+      Validators.email
+    ]),
+    phoneNumber: new FormControl('', [
+      Validators.required
+    ]),
+    subject: new FormControl('', [
+      Validators.required
+    ]),
+    text: new FormControl('', [
+      Validators.required
+    ]),
+  });
+
   title = 'Contact Us';
   content;
-
-  emailFormControl = new FormControl('', [Validators.required, Validators.email]);
-  fullName: string;
-  telephone: string;
-  date: FormControl;
-  contactEmail: string;
-  textarea: string;
-  subject: string;
+  genericErrorStateMatcher = new GenericErrorStateMatcher();
   alertMessage: AlertMessage;
   alertSubscription: Subscription;
 
@@ -42,7 +63,7 @@ export class ContactUsComponent implements OnInit {
   }
 
   onClick() {
-    this.content={'fullname':this.fullName,'email':this.contactEmail,'telephone':this.telephone,'subject':this.subject,'text':this.textarea};
+    //this.content={'fullname':this.fullName,'email':this.contactEmail,'telephone':this.telephone,'subject':this.subject,'text':this.textarea};
     this.contanctUsService.postDetails(this.content);
     this.alertService.success("Your message has been sent.")
   }
